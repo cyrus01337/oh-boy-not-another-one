7b8cf37c861d5c10f38ab0cbeace5c8fe4e4f887
cyrus01337 <cyrus21337@gmail.com>
2022-07-24 21:47:52 +0100

    Rework to support bot auto-roles

--- cogs/roles.py
+++ cogs/roles.py
@@ -1,23 +1,24 @@
-import discord
+from discord import Member, Role
 from discord.ext import commands
+from discord.ext.commands import Bot, Context
 
 
 class Roles(commands.Cog):
-    def __init__(self, bot: commands.Bot):
+    def __init__(self, bot: Bot):
         self.bot = bot
         self.reason = "Role auto-assignment"
 
         self.bot.loop.create_task(self.__ainit__())
 
     @property
-    def bots(self) -> discord.Role:
+    def bots(self) -> Role:
         return self.bot.guild.get_role(761422177798324244)
 
     @property
-    def community(self) -> discord.Role:
+    def community(self) -> Role:
         return self.bot.guild.get_role(761425977401933845)
 
-    def _resolve_roles_from_member(self, member: discord.Member) -> list[discord.Role]:
+    def _resolve_roles_from_member(self, member: Member) -> list[Role]:
         roles = [
             self.community
         ]
@@ -30,7 +31,7 @@ class Roles(commands.Cog):
     async def __ainit__(self):
         await self.bot.wait_until_ready()
 
-        members: list[discord.Member] = self.bot.guild.members
+        members: list[Member] = self.bot.guild.members
 
         for member in members:
             roles = self._resolve_roles_from_member(member)
@@ -38,7 +39,7 @@ class Roles(commands.Cog):
             await member.add_roles(*roles, reason=self.reason)
 
     @commands.Cog.listener()
-    async def on_member_join(self, member: discord.Member):
+    async def on_member_join(self, member: Member):
         roles = self._resolve_roles_from_member(member)
 
         await member.add_roles(roles, reason=self.reason)
